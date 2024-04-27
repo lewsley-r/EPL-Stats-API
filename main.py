5059543848e7357600a7e9c36c1ac39862beedaa
lewsley-r <lewsley-r@ulster.ac.uk>
2024-04-27 22:17:20 +0100

    Abstract an extra piece of logic into get_stat func, also added Assists endpoint

--- main.py
+++ main.py
@@ -11,20 +11,31 @@ def get_stat(endpoint_postfix):
         "user-agent": "mozilla/5.0 (x11; linux x86_64) applewebkit/537.36 (khtml, like gecko) chrome/123.0.0.0 safari/537.36"}
     resp = httpx.get(url, headers=headers)
     html = HTMLParser(resp.text)
-    return html
+    stat = html.css("tr.table__row")
+    return stat
 
 
 @app.get("/player/goals")
 def get_top_10_players_by_goals():
-    html_goals = get_stat('players/goals')
-
-    player_goals = html_goals.css("tr.table__row")
+    player_goals = get_stat('players/goals')
     players = []
     for player in player_goals:
         item = player.text().replace('\n', '')
-
         items = list(filter(None, item.split('  ')))
         player_profile = {'Rank': items[0], 'Player Name': items[1], 'Club': items[2],
                           'Nationality': items[3], 'Goals': items[4]}
+        players.append(player_profile)
+    return players
+
+
+@app.get("/player/assists")
+def get_top_10_players_by_assists():
+    player_assists = get_stat('players/goal_assist')
+    players = []
+    for player in player_assists:
+        item = player.text().replace('\n', '')
+        items = list(filter(None, item.split('  ')))
+        player_profile = {'Rank': items[0], 'Player Name': items[1], 'Club': items[2],
+                          'Nationality': items[3], 'Assists': items[4]}
         players.append(player_profile)
     return players
